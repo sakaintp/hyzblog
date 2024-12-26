0ee408211438c971a2a5e9a3cb792098a7be319a
heyizhao <heyizhao-it@bestpay.com.cn>
2024-12-26 17:56:56 +0800

    fix: fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {Providers} from "./providers";
-import { waitForDebugger } from "inspector";
 import React, { useState, useEffect } from 'react';
 
 const geistSans = Geist({
@@ -31,28 +30,25 @@ export default function RootLayout({
   const [theme, setTheme] = useState('light'); // 初始设置为浅色模式
 
   //主题跟随系统
-  const followSystem = () => {
-    useEffect(() => {
-      const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-      const handleChange = (e:any) => {
-        console.log('夜间模式为', e.matches ? '深色模式' : '浅色模式');
-        if (e.matches) {
-          setTheme('dark'); // 系统为深色模式
-        } else {
-          setTheme('light'); // 系统为浅色模式
-        }
-      };
-      // 初始检查
-      handleChange(mediaQuery); 
-      // 监听系统颜色模式变化
-      mediaQuery.addEventListener('change',handleChange);
-      return () => {
-        mediaQuery.removeEventListener('change',handleChange);
-      };
-    }, []);
-  }
-  //默认跟随系统
-  followSystem()
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleChange = (e:any) => {
+      console.log('夜间模式为', e.matches ? '深色模式' : '浅色模式');
+      if (e.matches) {
+        setTheme('dark'); // 系统为深色模式
+      } else {
+        setTheme('light'); // 系统为浅色模式
+      }
+    };
+    // 初始检查
+    handleChange(mediaQuery); 
+    // 监听系统颜色模式变化
+    mediaQuery.addEventListener('change',handleChange);
+    return () => {
+      mediaQuery.removeEventListener('change',handleChange);
+    };
+  }, []);
+  
   return (
     <html lang="en" className={theme}>
       <body
