a0a5558115651f268500727e5438623b31c83b49
heyizhao <17691160334@163.com>
2025-12-03 16:53:24 +0800

    fix: 修复组件引用问题

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button } from '@nextui-org/button'
+import { Button } from '@nextui-org/react'
 import { ThemeSwitch } from "@/components/ThemeSwitch"
 import { useRouter } from 'next/navigation'
 import StarSky from '@/components/StarSky'
